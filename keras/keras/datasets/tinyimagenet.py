109892c437f1a5842c90011a3c6988cf2966f01d
HunjaeJung <hunjaege@gmail.com>
2015-05-12 18:12:12 +0900

    dataset tinyimage validation code added

--- keras/keras/datasets/tinyimagenet.py
+++ keras/keras/datasets/tinyimagenet.py
@@ -31,7 +31,7 @@ def load_data(path):
     X_train = np.zeros((train_size, 3, 64, 64), dtype="uint8")
     y_train = np.zeros((train_size,), dtype="str")
     X_val = np.zeros((val_size, 3, 64, 64), dtype="uint8") # TODO
-    y_val = np.zeros((val_size,), dtype="uint8")
+    y_val = np.zeros((val_size,), dtype="str")
 
     path_train = os.path.join(path, 'train')
     path_val = os.path.join(path, 'val')
@@ -50,11 +50,28 @@ def load_data(path):
         y_train[idx] = label
 
     # change text label(n04366367, ...) to (0, 1, 2, ...)
-    print "encoding labels..."
+    print "encoding labels for training data..."
     le = LabelEncoder()
     y_train = le.fit_transform(y_train)
 
     #TODO load validation set from tiny-imagenet
+    for idx, (label, img) in enumerate(read_files(path_val)):
+        # reshape (64, 64, 3) -> (3, 64, 64)
+        # gray color image is combined ... e.g. n04366367_182.JPEG
+        # Grey-scale means that all values have the same intensity. Set all channels
+        # (in RGB) equal to the the grey value and you will have the an RGB black and
+        # white image.
+        if img.ndim == 2:
+            img = np.array([img[:, :], img[:, :], img[:, :]])
+        elif img.ndim == 3:
+            img = np.array([img[:, :, 0], img[:, :, 1], img[:, :, 2]])
+        X_val[idx, :, :, :] = img
+        y_val[idx] = label
+
+    # change text label(n04366367, ...) to (0, 1, 2, ...)
+    print "encoding labels for validation data..."
+    le = LabelEncoder()
+    y_val = le.fit_transform(y_val)
 
     return (X_train, y_train), (X_val, y_val)
 
